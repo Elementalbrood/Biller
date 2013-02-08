9afd3f56aa72955d5ebc541b466d8d52ed70122e
Sean Connelly <falcon725@hotmail.com>
2013-02-07 19:07:00 -0600

    Edited the Edit Bill GUI - Dad also brought up that we should indeed make a Biweekly occurence.

--- Biller/EditBill.Designer.cs
+++ Biller/EditBill.Designer.cs
@@ -38,6 +38,15 @@ namespace Biller
 		{
 			this.label1 = new System.Windows.Forms.Label();
 			this.comboBox_editBill = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox_editBillBalance = new System.Windows.Forms.TextBox();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton4 = new System.Windows.Forms.RadioButton();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -53,22 +62,122 @@ namespace Biller
 			// comboBox_editBill
 			// 
 			this.comboBox_editBill.FormattingEnabled = true;
-			this.comboBox_editBill.Location = new System.Drawing.Point(88, 23);
+			this.comboBox_editBill.Location = new System.Drawing.Point(132, 23);
 			this.comboBox_editBill.Name = "comboBox_editBill";
-			this.comboBox_editBill.Size = new System.Drawing.Size(197, 21);
+			this.comboBox_editBill.Size = new System.Drawing.Size(203, 21);
 			this.comboBox_editBill.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+			this.label2.Location = new System.Drawing.Point(16, 73);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(63, 21);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Balance:";
+			// 
+			// textBox_editBillBalance
+			// 
+			this.textBox_editBillBalance.Location = new System.Drawing.Point(132, 72);
+			this.textBox_editBillBalance.Name = "textBox_editBillBalance";
+			this.textBox_editBillBalance.Size = new System.Drawing.Size(203, 20);
+			this.textBox_editBillBalance.TabIndex = 3;
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(132, 123);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(203, 20);
+			this.dateTimePicker1.TabIndex = 4;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(16, 127);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(74, 25);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Due Date:";
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+			this.label4.Location = new System.Drawing.Point(16, 177);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(88, 20);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Reoccurence:";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(27, 221);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(88, 26);
+			this.radioButton1.TabIndex = 7;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "none";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(230, 219);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(105, 30);
+			this.radioButton2.TabIndex = 8;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "Monthly";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.Location = new System.Drawing.Point(27, 292);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(88, 29);
+			this.radioButton3.TabIndex = 9;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "Biannual";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// radioButton4
+			// 
+			this.radioButton4.Location = new System.Drawing.Point(230, 286);
+			this.radioButton4.Name = "radioButton4";
+			this.radioButton4.Size = new System.Drawing.Size(105, 40);
+			this.radioButton4.TabIndex = 10;
+			this.radioButton4.TabStop = true;
+			this.radioButton4.Text = "Annual";
+			this.radioButton4.UseVisualStyleBackColor = true;
 			// 
 			// EditBill
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(421, 398);
+			this.Controls.Add(this.radioButton4);
+			this.Controls.Add(this.radioButton3);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.textBox_editBillBalance);
+			this.Controls.Add(this.label2);
 			this.Controls.Add(this.comboBox_editBill);
 			this.Controls.Add(this.label1);
 			this.Name = "EditBill";
 			this.Text = "EditBill";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.RadioButton radioButton4;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.TextBox textBox_editBillBalance;
+		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.ComboBox comboBox_editBill;
 		private System.Windows.Forms.Label label1;
 	}
